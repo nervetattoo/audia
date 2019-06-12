227ab4a09fe5a14529453c41ebbc4cfa4e54aea3
Raymond Julin <raymond.julin@gmail.com>
2019-06-12 23:51:17 +0200

    Add basic upload support

--- routes/songs.js
+++ routes/songs.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const getDb = require('../lib/db');
 const nanoid = require('nanoid');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'data/songs/');
+  },
+  filename(req, file, cb) {
+    cb(null, req.songId);
+  },
+});
+const upload = multer({ storage });
 
 const router = express.Router();
 
 const db = getDb('songs', { valueEncoding: 'json' });
+
+function generateSongId(req, res, next) {
+  req.songId = nanoid();
+  next(null);
+}
 
 function encodeNdjson(data) {
   return JSON.stringify(data) + '\n';
@@ -20,8 +36,12 @@ function listSongs(req, res) {
 }
 
 async function addSong(req, res) {
-  const id = `song:${nanoid()}`;
-  await db.put(id, req.body.metadata);
+  const id = `songs:${req.songId}`;
+  const data = {
+    meta: req.body.metadata,
+    path: req.file.path,
+  };
+  await db.put(id, data);
 
   res.send({ id });
 }
@@ -30,6 +50,6 @@ async function addSong(req, res) {
 router
   .route('/')
   .get(listSongs)
-  .post(addSong);
+  .post(generateSongId, upload.single('file'), addSong);
 
 module.exports = router;
