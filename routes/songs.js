8395f071bbd26e2707bc5388211f71a34a251b69
Raymond Julin <raymond.julin@gmail.com>
2019-06-12 23:36:43 +0200

    Basic endpoint to add a song (metadata) and list all songs

--- routes/songs.js
+++ routes/songs.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const getDb = require('../lib/db');
+const nanoid = require('nanoid');
+
 const router = express.Router();
 
-/* GET songs listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
-});
+const db = getDb('songs', { valueEncoding: 'json' });
+
+function encodeNdjson(data) {
+  return JSON.stringify(data) + '\n';
+}
+
+function listSongs(req, res) {
+  db.createReadStream()
+    .on('data', data => {
+      res.write(encodeNdjson(data));
+    })
+    .on('end', () => res.end())
+    .on('close', () => res.end());
+}
+
+async function addSong(req, res) {
+  const id = `song:${nanoid()}`;
+  await db.put(id, req.body.metadata);
+
+  res.send({ id });
+}
+
+// Index endpoints
+router
+  .route('/')
+  .get(listSongs)
+  .post(addSong);
 
 module.exports = router;
